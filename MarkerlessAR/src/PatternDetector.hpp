058c82a5fd4755d62609fd3d3642df897e8206f6
Oliver <oliver.wilkie@gmail.com>
2013-07-04 10:00:06 +0100

    Changed how Orb DescriptorExtractor is created to allow control of parameters

--- MarkerlessAR/src/PatternDetector.hpp
+++ MarkerlessAR/src/PatternDetector.hpp
@@ -28,7 +28,7 @@ public:
     PatternDetector
         (
         cv::Ptr<cv::FeatureDetector>     detector  = new cv::ORB(1000), 
-        cv::Ptr<cv::DescriptorExtractor> extractor = cv::DescriptorExtractor::create("ORB"),
+        cv::Ptr<cv::DescriptorExtractor> extractor = new cv::ORB(1000),
         cv::Ptr<cv::DescriptorMatcher>   matcher   = new cv::BFMatcher(cv::NORM_HAMMING, true),
         bool enableRatioTest                       = false
         );
