9ad6e7bd1289707df7e32d36bab2b91b6b86aa1b
Oliver <oliver.wilkie@gmail.com>
2013-07-25 09:51:03 +0100

    Refactored JNI Code now to make it easier to read. Follows better best practices!

---
+++ MarkerlessARForAndroid/jni/NativeFrameProcessor_jni.cpp
@@ -0,0 +1,49 @@
+#include <jni.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include "CameraCalibration.hpp"
+#include "ARPipeline.hpp"
+#include "ARDrawingContext.hpp"
+
+using namespace std;
+using namespace cv;
+
+extern "C" {
+
+	JNIEXPORT jlong JNICALL
+	Java_org_opencv_markerlessarforandroid_NativeFrameProcessor_nativeCreateObject
+	(JNIEnv *env, jobject obj, jlongArray images, jint imgCount, jfloat fx, jfloat fy, jfloat cx, jfloat cy)
+	{
+		CameraCalibration callib(fx, fy, cx, cy);
+
+		jlong imagesBuff[imgCount];
+		env->GetLongArrayRegion(images, 0, imgCount, imagesBuff);
+
+		vector<Mat> arr(imgCount);
+		for (int i = 0; i < imgCount; i++) {
+			Mat *image;
+			image = (Mat *)imagesBuff[i];
+			arr[i] = *image;
+		}
+
+		return (long) new ARPipeline(arr, callib);
+	}
+
+	JNIEXPORT void JNICALL
+	Java_org_opencv_markerlessarforandroid_NativeFrameProcessor_nativeDestroyObject
+	(JNIEnv *env, jobject obj, jlong object) {
+		delete (ARPipeline *) object;
+	}
+
+	JNIEXPORT jint JNICALL
+	Java_org_opencv_markerlessarforandroid_NativeFrameProcessor_nativeProcess
+	(JNIEnv *env, jobject obj, jlong object, jlong frame)
+	{
+		ARPipeline *pipeline = (ARPipeline *)object;
+		Mat *mat = (Mat *)frame;
+
+		bool isFound = pipeline->processFrame(*mat);
+		return isFound ? 1 : 0;
+	}
+}
